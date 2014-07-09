0c27bd66753f88398ff1be09ce52869b8674a69a
Igor Hlina <srigi@srigi.sk>
2014-07-09 14:35:20 +0200

    Simple TODO app

--- hello-world/myapp/controllers.js
+++ hello-world/myapp/controllers.js
@@ -1,13 +1,29 @@
 angular.module('myapp')
 
-.controller('UserController', function($scope, $timeout) {
+.controller('TaskController', function($scope) {
   'use strict';
 
-  $scope.username = 'Anicka';
-
-
-  this.setUsername = function(username) {
-    $scope.username = username || 'srigi';
+  $scope.data = {
+    tasks: [
+      {"text":"Buy beer","done":false,"tags":["home","urgent"]},
+      {"text":"Send spreadsheet","done":true,"tags":["work","urgent"]},{"text":"Send spreadsheet","done":true,"tags":["work","urgent"]},{"text":"Call Jim","done":true,"tags":["work"]},{"text":"Recharge credit card","done":true,"tags":["wife","urgent"]},{"text":"Print monthly report","done":true,"tags":["work","urgent"]},{"text":"Print monthly report","done":true,"tags":["work","urgent"]}]
   };
 
+  this.saveNewTask = function() {
+    $scope.data.tasks.unshift({
+      text: $scope.text,
+      done: true,
+      tags: [],
+    });
+  };
+
+  this.getDoneCount = function() {
+    return _.reduce($scope.data.tasks, function(memo, task) {
+      if (task.done) {
+        return memo + 1
+      } else {
+        return memo
+      }
+    }, 0);
+  }
 });
