de2208805e942f2cbd4b7ffd7f22da288d787892
Igor Hlina <srigi@srigi.sk>
2014-07-09 11:52:05 +0200

    Minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp          = require('gulp');
 var livereload    = require('gulp-livereload');
+var ngAnotate     = require('gulp-ng-annotate');
 var runSequence   = require('run-sequence');
+var uglify        = require('gulp-uglify');
 var watch         = require('gulp-watch');
 
 
@@ -15,6 +17,12 @@ gulp.task('livereload', function() {
     ]).on('change', livereload.changed);
 });
 
+gulp.task('minify', function() {
+  return gulp.src('hello-world/**/*.js')
+    .pipe(ngAnotate())
+    .pipe(uglify())
+    .pipe(gulp.dest('hello-world/build'));
+})
 
 // === Main tasks definitions ===
 //
