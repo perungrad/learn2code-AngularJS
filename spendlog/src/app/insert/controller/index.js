5846334466222d69a376410798d32070c2d42809
Igor Hlina <srigi@srigi.sk>
2014-07-10 11:55:45 +0200

    Sliders on insert page

--- spendlog/src/app/insert/controller/index.js
+++ spendlog/src/app/insert/controller/index.js
@@ -3,5 +3,19 @@ angular.module('sl.insert')
 .controller('Insert.IndexController', function($scope) {
   'use strict';
 
-  $scope.foo = 'bar';
+  $scope.data = {
+    volumeTotal: 14.7,
+    volumeX10: undefined,
+    volumeX1: undefined,
+    volumeX01: undefined,
+  };
+
+
+  $scope.$watchGroup(['data.volumeX10', 'data.volumeX1', 'data.volumeX01'], function() {
+    $scope.data.volumeTotal = ($scope.data.volumeX10 * 10) + ($scope.data.volumeX1 * 1) + ($scope.data.volumeX01 * 0.1);
+  });
+
+  $scope.data.volumeX10 = parseInt(($scope.data.volumeTotal % 100) / 10);
+  $scope.data.volumeX1  = parseInt(($scope.data.volumeTotal % 10)  / 1);
+  $scope.data.volumeX01 = parseInt(($scope.data.volumeTotal * 10 % 10));
 });
