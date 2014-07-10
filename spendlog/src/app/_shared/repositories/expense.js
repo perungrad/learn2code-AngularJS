c2cf54ac32541ab30b48e07d4ca0f7629ca254b1
Igor Hlina <srigi@srigi.sk>
2014-07-10 16:16:13 +0200

    Load more functionality

--- spendlog/src/app/_shared/repositories/expense.js
+++ spendlog/src/app/_shared/repositories/expense.js
@@ -1,6 +1,6 @@
 angular.module('sl.shared')
 
-.service('ExpensesRepository', function(ExpensesResource) {
+.service('ExpensesRepository', function($q, ExpensesResource) {
   'use strict';
 
   this._queryCache = {};
@@ -14,6 +14,24 @@ angular.module('sl.shared')
     return this._queryCache.getList;
   };
 
+  this.loadMore = function() {
+    var dfd = $q.defer();
+    var listPromise = this.getList();
+
+    listPromise.then(function(list) {
+      var countListItems = list.data.expenses.length;
+      var loadMorePromise = ExpensesResource.get({from: countListItems + 1}).$promise;
+      listPromise = loadMorePromise;
+
+      loadMorePromise.then(function(moreList) {
+        list.data.expenses.push.apply(list.data.expenses, moreList.data.expenses);
+        list.meta.hasMore = moreList.meta.hasMore;
+        dfd.resolve(moreList);
+      });
+    });
+
+    return dfd.promise;
+  };
 
   this.storeExpense = function(volume) {
     var request = ExpensesResource.storeExpense({volume:volume});
--- spendlog/src/app/expenses/controllers/index.js
+++ spendlog/src/app/expenses/controllers/index.js
@@ -3,6 +3,10 @@ angular.module('sl.expenses')
 .controller('Expenses.IndexController', function($scope, ExpensesRepository) {
   'use strict';
 
+  this.loadMore = function() {
+    ExpensesRepository.loadMore();
+  };
+
   ExpensesRepository.getList().then(function(list) {
     $scope.data = list.data;
     $scope.meta = list.meta;
