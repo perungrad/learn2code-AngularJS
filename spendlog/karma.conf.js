5edcc81ae8c968a57918b59e5382fbcb92c7d067
Igor Hlina <srigi@srigi.sk>
2014-07-11 12:24:40 +0200

    Rearrange assets to by in more consistent order

--- spendlog/karma.conf.js
+++ spendlog/karma.conf.js
@@ -11,9 +11,11 @@ module.exports = function(config) {
       'src/vendor/angular-animate/angular-animate.js',
       'src/vendor/angular-resource/angular-resource.js',
       'src/vendor/angular-route/angular-route.js',
+
+      'src/vendor/angular-growl-v2/build/angular-growl.js',
       'src/vendor/angular-strap/dist/angular-strap.js',
       'src/vendor/angular-strap/dist/angular-strap.tpl.js',
-      'src/vendor/angular-growl-v2/build/angular-growl.js',
+
       'src/vendor/angular-mocks/angular-mocks.js',
 
       { pattern: 'src/images/**/*.{gif,jpg,jpeg,png,webp}', included:false, served:true, watched:false},
